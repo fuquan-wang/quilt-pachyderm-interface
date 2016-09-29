48fa1e11cfc22d6e9faa59545c69cb6b9822d7d0
Fuquan Wang <fqwang.hep@gmail.com>
2016-09-29 18:13:11 +0000

    fuquan-wang: clean-up the old architecture

--- obsolete/jobDriver/driver.py
+++ obsolete/jobDriver/driver.py
@@ -38,6 +38,10 @@ def get_parser():
 		type=str,
 		default=str(uuid.uuid4()),
 		help='The desired docker image name, a UUID will be used as default')
+	parser.add_argument('--parallelism',
+		type=int,
+		default=4,
+		help='The number of workers, default is 4')
 	return parser
 
 if __name__ == '__main__':
@@ -48,26 +52,37 @@ if __name__ == '__main__':
 	f.download()
 	filename = str(f.filename)
 	s = filename.replace(".","_")
-	pipeline_name = 'PROC_'+s
 	subprocess.call(['pachctl','create-repo',s])
 	subprocess.call(['pachctl','start-commit',s,'master'])
 	subprocess.call(['pachctl','put-file',s,'master','data','-f',filename])
 	subprocess.call(['pachctl','finish-commit',s,'master'])
+	p = subprocess.Popen('wc -l '+filename+' |awk \'{print $1\'}', stdout=subprocess.PIPE, shell=True)
+	(nlines, err) = p.communicate()
 	subprocess.call(['rm','-f',filename])
+
+	split_pipeline = 'SPLIT_'+s
+	nfiles_per_job = 4
+	subprocess.call(['python','../splitter/splitter.py',
+			'--output_dir', args.output_dir, 
+			'--nlines', str(int(nlines)/args.parallelism/nfiles_per_job+1),
+			'--input_repo', s,
+			'--pipeline', split_pipeline])
+
+	proc_pipeline = 'PROC_'+s
 	subprocess.call(['python','../imageCreator/generate.py',
 			'--output_dir', args.output_dir, 
 			'--req_file', args.req_file,
 			'--tar_ball', args.tar_ball,
 			'--script_name', args.script_name, 
-			'--input_repo', s,
+			'--input_repo', split_pipeline,
 			'--docker_user', args.docker_user,
-			'--pipeline', pipeline_name,
+			'--pipeline', proc_pipeline,
 			'--docker_image', args.docker_image,
-			'--parallelism', '1'])
+			'--parallelism', str(args.parallelism)])
 
 	jobid=''
 	while jobid=='':
-		p = subprocess.Popen('pachctl list-job -p '+pipeline_name+' |grep running |awk \'{print $1}\'', stdout=subprocess.PIPE, shell=True)
+		p = subprocess.Popen('pachctl list-job -p '+proc_pipeline+' |grep running |awk \'{print $1}\'', stdout=subprocess.PIPE, shell=True)
 		(jobid, err) = p.communicate()
 		time.sleep(5)
 	jobid = jobid[:-1]
@@ -75,12 +90,19 @@ if __name__ == '__main__':
 	print 'waiting for job', jobid, 'to finish'
 	subprocess.call(['pachctl','inspect-job',jobid,'--block'])
 	print 'job', jobid, 'has finished'
-	p = subprocess.Popen('pachctl list-commit '+pipeline_name+' |tail -1 |awk \'{print $2}\'', stdout=subprocess.PIPE, shell=True)
+
+	merge_pipeline = 'MERGE_'+s
+	subprocess.call(['python','../merger/merger.py',
+			'--output_dir', args.output_dir, 
+			'--input_repo', proc_pipeline,
+			'--pipeline', merge_pipeline])
+
+	p = subprocess.Popen('pachctl list-commit '+merge_pipeline+' |tail -1 |awk \'{print $2}\'', stdout=subprocess.PIPE, shell=True)
 	(outputid, err) = p.communicate()
 	outputid = outputid[ outputid.index('/')+1 : -1 ]
-	print 'pachctl','get-file',pipeline_name,outputid,'data','>','merge_'+s
-	p = subprocess.Popen('pachctl get-file '+pipeline_name+' '+outputid+' data > merge_'+s, stdout=subprocess.PIPE, shell=True)
+	print 'pachctl','get-file',merge_pipeline,outputid,'data','>','merge_'+s
+	p = subprocess.Popen('pachctl get-file '+merge_pipeline+' '+outputid+' data > merge_'+s, stdout=subprocess.PIPE, shell=True)
 	p.communicate()
 	f = connection.upload('merge_'+s)
 	print f.url
-	susprocess.call(['rm','-f','merge_'+s])
+	subprocess.call(['rm','-f','merge_'+s])
---
+++ obsolete/splitter/splitter.py
@@ -0,0 +1,54 @@
+import argparse, sys, os
+import subprocess
+import quilt
+import uuid
+import time
+
+def get_parser():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--output_dir',
+		type=str,
+		required=True,
+		help='The file output directory')
+	parser.add_argument('--nlines',
+		type=int,
+		default=10000,
+		help='The number of lines per file')
+	parser.add_argument('--input_repo',
+		type=str,
+		required=True,
+		help='The name of the input repo')
+	parser.add_argument('--pipeline',
+		type=str,
+		required=True,
+		help='The name of the pipeline')
+	return parser
+
+if __name__ == '__main__':
+	args = get_parser().parse_args()
+	output_dir = args.output_dir
+	if not os.path.exists( output_dir ):
+		os.makedirs( output_dir )
+
+	f = open(output_dir+'/split.json', 'w')
+	f.write('{\n\t"pipeline": {\n\t\t"name": "'+args.pipeline+'"\n\t},\n');
+	f.write('\t"transform": {\n\t\t"cmd": [ "sh" ],\n\t\t"stdin": [\n\t\t\t"split -d -a 10 -l '+str(args.nlines)+' /pfs/'+args.input_repo+'/data /pfs/out/data_"\n\t\t],\n\t\t"image": "fuquanwang/pach-job-shim:latest"\n\t},\n')
+	f.write('\t"parallelism_spec": {\n\t\t"strategy": "CONSTANT",\n\t\t"constant": 1\n\t},\n')
+	f.write('\t"inputs": [\n\t\t{\n\t\t\t"repo": {\n\t\t\t\t"name": "'+args.input_repo+'"\n\t\t\t}\n\t\t}\n\t]\n}\n')
+	f.close()
+
+	subprocess.call(['pachctl', 'create-pipeline', '-f', output_dir+'/split.json'])
+
+	wait_time=120
+	p = subprocess.Popen('pachctl list-job -p '+args.pipeline+' |grep running |awk \'{print $1}\'', stdout=subprocess.PIPE, shell=True)
+	(jobid, err) = p.communicate()
+	while jobid=='' and wait_time>0:
+		p = subprocess.Popen('pachctl list-job -p '+args.pipeline+' |grep running |awk \'{print $1}\'', stdout=subprocess.PIPE, shell=True)
+		wait_time -= 5
+		time.sleep(5)
+		(jobid, err) = p.communicate()
+
+	if( jobid!='' ):
+		print 'waiting for job', jobid, 'to finish'
+		subprocess.call(['pachctl','inspect-job',jobid,'--block'])
+		print 'job', jobid, 'has finished'
